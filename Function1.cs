144293ca09896de9831e566737b7624ed18f9e20
John.michael <John.michael@studentambassadors.com>
2021-08-04 08:24:01 -0500

    added a output binding

--- Function1.cs
+++ Function1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -7,10 +8,13 @@ namespace AzureFunctiom_timer
 {
     public static class Function1
     {
+
         [FunctionName("Function1")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer,  
+            [ Queue("theeque", Connection= "AzureWebsStorage")] out string QMessage, ILogger log)
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now.ToLongTimeString()}");
+            QMessage = "Timmer triggered";
         }
     }
 }
